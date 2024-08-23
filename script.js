a47743393e35c67d7dec042fea117a01ffd866e8
Hào Lâm <haolamnm@gmail.com>
2024-08-23 15:12:20 +0700

    Final Update for script.js
    This website was created around July for a project, and I want to archive it on my main account.

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById('registrationForm');
-    form.addEventListener('submit', function (e) {
-      e.preventDefault();
-      var name = document.getElementById('name').value;
-      if (/\\d/.test(name)) {
-        alert('Tên không được chứa số.');
-        return false;
-      }
-  
-      var phone = document.getElementById('phone').value;
-      if (!/^[0-9]+$/.test(phone)) {
-        alert('Số điện thoại chỉ được chứa số.');
-        return false;
-      }
-  
-      var email = document.getElementById('email').value;
-      if (!/@/.test(email)) {
-        alert('Email phải chứa ký tự @.');
-        return false;
-      } else {
-        alert('Email bạn nhập sẽ là email được nhận những thông tin chính thức sắp đến từ dự án');
-      }
-  
-      // Here you would normally handle the form submission,
-      // for example sending data to a server or opening the Google Form link.
-      // window.location.href = 'YOUR_GOOGLE_FORM_LINK';
-  
-      return true;
-    });
+if (!sessionStorage.getItem("reloaded")) {
+    // Set the reload flag
+    sessionStorage.setItem("reloaded", true);
+    setTimeout(function() {
+      location.reload();
+    }, 500); // auto reload time
+  }
+
+window.addEventListener('DOMContentLoaded', function() {
+    // Set a timeout to hide the preloader after 3 seconds
+    var mainContent = document.getElementById('content');
+    setTimeout(function() {
+      document.getElementById('preloader').style.display = 'none';
+      document.getElementById('content').style.visibility = 'visible';
+      mainContent.classList.add('fade-in');
+    }, 3000); // 3000 milliseconds = 3 seconds
   });
-  
